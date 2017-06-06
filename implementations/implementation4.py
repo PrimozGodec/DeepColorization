863f3f5885a54bc8003e52860d122c5750b71b55
primozgodec <p.godec9@gmail.com>
2017-06-06 09:06:24 +0200

    Batch size changed to 8

--- implementations/implementation4.py
+++ implementations/implementation4.py
@@ -20,7 +20,7 @@ from implementations.support_scripts.image_processing import load_images, images
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-b_size = 16
+b_size = 8
 dir_name = "../small_dataset"
 list_dir = os.listdir(dir_name)
 shuffle(list_dir)
