081e561bc26b5b49d7989183a6719c20d59516f9
primozgodec <p.godec9@gmail.com>
2017-06-09 10:55:56 +0200

    Implemented analizer to show the history -- need to be tested when data available

---
+++ implementations/support_scripts/analizer.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+"""
+This script show data chart analysis for training
+"""
+
+# select implementation
+import os
+import pickle
+
+implementation = "imp7"
+
+# list all history files
+files = sorted([x for x in os.listdir("../../history") if x.startswith(implementation)])
+
+loss = []
+val_loss = []
+for file in files:
+    with open(file) as f:
+        data = pickle.load(f)
+        loss += data["loss"]
+        val_loss += data["val_loss"]
+
+plt.plot(loss)
+plt.plot(val_loss)
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
