8081138280a252763f759358f1629b791a532afa
primozgodec <p.godec9@gmail.com>
2017-06-12 12:16:56 +0200

    Sanity check

--- implementations/implementation7l-sc.py
+++ implementations/implementation7l-sc.py
@@ -88,7 +88,7 @@ model.summary()
 
 start_from = 100
 save_every_n_epoch = 10
-n_epochs = 300
+n_epochs = 3000
 
 model.load_weights("../weights/implementation7l-110.h5")
 
@@ -96,7 +96,7 @@ g = image_processing.image_generator_parts(list_dir, b_size, im_size=(224, 224))
 
 for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
     print("START", i * save_every_n_epoch, "/", n_epochs)
-    history = model.fit_generator(g, steps_per_epoch=len(list_dir)//b_size, epochs=save_every_n_epoch)
+    history = model.fit_generator(g, steps_per_epoch=1000, epochs=save_every_n_epoch)
     # model.save_weights("../weights/implementation7l-" + str(i * save_every_n_epoch) + ".h5")
 
     # save sample images
