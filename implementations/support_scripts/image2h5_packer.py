b65d99a14e06455ac2af1d20a328df087c26027c
primozgodec <p.godec9@gmail.com>
2017-06-14 16:39:41 +0200

    Increase number of kept h5 files

--- implementations/support_scripts/image2h5_packer.py
+++ implementations/support_scripts/image2h5_packer.py
@@ -65,7 +65,7 @@ class ImagePacker(threading.Thread):
         k = len(self.prefix)
         only_files = get_files()
 
-        keep_files = 300
+        keep_files = 600
 
         while len(only_files) > keep_files and only_files[0] != self.current_file:
             os.remove(os.path.join(self.dir_to, only_files[0]))
