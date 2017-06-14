48707af183d4b1050df195d98f45e96c903befcc
primozgodec <p.godec9@gmail.com>
2017-06-14 22:18:10 +0200

    Imp7d-reg on gpu 3

--- implementations/implementation7withdownload-reg.py
+++ implementations/implementation7withdownload-reg.py
@@ -13,7 +13,7 @@ from keras import backend as K, Input
 from keras import optimizers
 from keras.layers import Conv2D, UpSampling2D, Lambda, Dense, Merge, merge, concatenate, regularizers
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 
 b_size = 32
 
