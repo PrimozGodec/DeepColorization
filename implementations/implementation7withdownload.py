511f6952723e7adfd5814d7572edad2db661115d
primozgodec <p.godec9@gmail.com>
2017-06-16 11:37:38 +0200

    Imp7d returned in its original shape and test with overlapping implemented

--- implementations/implementation7withdownload.py
+++ implementations/implementation7withdownload.py
@@ -15,7 +15,7 @@ from keras import optimizers
 from keras.layers import Conv2D, UpSampling2D, Lambda, Dense, Merge, merge, concatenate
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 b_size = 32
 
@@ -102,18 +102,16 @@ g = h5_small_vgg_generator(b_size, "../h5_data", ip)
 gval = h5_small_vgg_generator(b_size, "../h5_validate", None)
 
 
-# for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
-#     print("START", i * save_every_n_epoch, "/", n_epochs)
-#     history = model.fit_generator(g, steps_per_epoch=60000/b_size, epochs=save_every_n_epoch,
-#                                   validation_data=gval, validation_steps=(128//b_size))
-#     model.save_weights("../weights/implementation7d-" + str(i * save_every_n_epoch) + ".h5")
-#
-#     # save sample images
-#     whole_image_check(model, 20, "imp7d-" + str(i * save_every_n_epoch) + "-")
-#
-#     # save history
-#     output = open('../history/imp7d-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
-#     pickle.dump(history.history, output)
-#     output.close()
+for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
+    print("START", i * save_every_n_epoch, "/", n_epochs)
+    history = model.fit_generator(g, steps_per_epoch=60000/b_size, epochs=save_every_n_epoch,
+                                  validation_data=gval, validation_steps=(128//b_size))
+    model.save_weights("../weights/implementation7d-" + str(i * save_every_n_epoch) + ".h5")
 
-whole_image_check_overlapping(model, 2, "imp7d-t" + "-")
+    # save sample images
+    whole_image_check(model, 20, "imp7d-" + str(i * save_every_n_epoch) + "-")
+
+    # save history
+    output = open('../history/imp7d-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
+    pickle.dump(history.history, output)
+    output.close()
