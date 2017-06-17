b0ceaa28a9df11f4ff1ec681711d75e7635933ac
primozgodec <p.godec9@gmail.com>
2017-06-17 11:51:50 +0200

    Implementation8 introduced

--- implementations/implementation8.py
+++ implementations/implementation8.py
@@ -3,40 +3,39 @@ import sys
 import os
 
 sys.path.append(os.getcwd()[:os.getcwd().index('implementations')])
-from implementations.support_scripts.common import make_prediction_sample, make_prediction_sample_part, whole_image_check
+
+from implementations.support_scripts.common import whole_image_check, h5_small_vgg_generator
 from keras.applications import VGG16
 from keras.engine import Model
 
-from implementations.support_scripts import image_processing
 from keras import backend as K, Input
 from keras import optimizers
-from keras.layers import Conv2D, UpSampling2D, Lambda, Dense, Merge, merge, concatenate
-from random import shuffle
+from keras.layers import Conv2D, UpSampling2D, Lambda, Dense, Merge, merge, concatenate, regularizers
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-b_size = 16
-dir_name = "../small_dataset"
-list_dir = os.listdir(dir_name)
-shuffle(list_dir)
-list_dir = list_dir
-num_classes = 40
+b_size = 32
+
 input_shape = (32, 32, 1)
 
 # main network
 main_input = Input(shape=input_shape, name='image_part_input')
 
-x = Conv2D(64, (3, 3), strides=(2, 2), padding="same", activation="relu")(main_input)
-# x = Conv2D(128, (3, 3), padding="same", activation="relu")(x)
+x = Conv2D(64, (3, 3), strides=(2, 2), padding="same", activation="relu",
+           kernel_regularizer=regularizers.l2(0.01))(main_input)
+x = Conv2D(128, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(x)
 
-x = Conv2D(128, (3, 3), strides=(2, 2), padding="same", activation="relu")(x)
-# x = Conv2D(256, (3, 3), padding="same", activation="relu")(x)
+x = Conv2D(128, (3, 3), strides=(2, 2), padding="same", activation="relu",
+           kernel_regularizer=regularizers.l2(0.01))(x)
+x = Conv2D(256, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(x)
 
-x = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu")(x)
-# x = Conv2D(512, (3, 3), padding="same", activation="relu")(x)
+x = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu",
+           kernel_regularizer=regularizers.l2(0.01))(x)
+x = Conv2D(512, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(x)
 
-x = Conv2D(512, (3, 3), padding="same", activation="relu")(x)
-main_output = Conv2D(256, (3, 3), padding="same", activation="relu")(x)
+x = Conv2D(512, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(x)
+main_output = Conv2D(256, (3, 3), padding="same", activation="relu",
+                     kernel_regularizer=regularizers.l2(0.01))(x)
 
 # VGG
 vgg16 = VGG16(weights="imagenet", include_top=True)
@@ -58,49 +57,60 @@ merged = concatenate([vgg_output, main_output], axis=3)
 last = Conv2D(128, (3, 3), padding="same")(merged)
 
 last = UpSampling2D(size=(2, 2))(last)
-last = Conv2D(64, (3, 3), padding="same", activation="sigmoid")(last)
-# last = Conv2D(64, (3, 3), padding="same", activation="sigmoid")(last)
+last = Conv2D(64, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(last)
+last = Conv2D(64, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(last)
 
 last = UpSampling2D(size=(2, 2))(last)
-# last = Conv2D(32, (3, 3), padding="same", activation="sigmoid")(last)
-last = Conv2D(2, (3, 3), padding="same", activation="sigmoid")(last)
+last = Conv2D(32, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(last)
+last = Conv2D(2, (3, 3), padding="same", activation="relu", kernel_regularizer=regularizers.l2(0.01))(last)
+
 
 def resize_image(x):
     return K.resize_images(x, 2, 2, "channels_last")
+
 
 def unormalise(x):
     # outputs in range [0, 1] resized to range [-100, 100]
     return (x * 200) - 100
 
+
 last = Lambda(resize_image)(last)
 last = Lambda(unormalise)(last)
 
 
+def custom_mse(y_true, y_pred):
+    return K.mean(K.square(y_pred - y_true), axis=[1, 2, 3])
+
+
 model = Model(inputs=[main_input, vgg16.input], output=last)
 opt = optimizers.Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-model.compile(optimizer=opt, loss="mean_squared_error")
+model.compile(optimizer=opt, loss=custom_mse)
 
 model.summary()
 
 start_from = 0
-save_every_n_epoch = 10
-n_epochs = 100
+save_every_n_epoch = 5
+n_epochs = 10000
+# model.load_weights("../weights/implementation7d-reg-55.h5")
 
+# start image downloader
+ip = None
 
-g = image_processing.image_generator_parts(list_dir, b_size, im_size=(224, 224))
+g = h5_small_vgg_generator(b_size, "../h5_data", ip)
+gval = h5_small_vgg_generator(b_size, "../h5_validate", None)
+
 
 for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
     print("START", i * save_every_n_epoch, "/", n_epochs)
-    history = model.fit_generator(g, steps_per_epoch=len(list_dir)//b_size, epochs=save_every_n_epoch)
+    history = model.fit_generator(g, steps_per_epoch=60000/b_size, epochs=save_every_n_epoch,
+                                  validation_data=gval, validation_steps=(128//b_size))
     model.save_weights("../weights/implementation8-" + str(i * save_every_n_epoch) + ".h5")
 
     # save sample images
-    whole_image_check(model, 20, "imp8-" + str(i) + "-")
+    whole_image_check(model, 20, "imp8-" + str(i * save_every_n_epoch) + "-")
 
     # save history
-    output = open('../history/imp8-{:0=4d}.pkl'.format(i), 'wb')
+    output = open('../history/imp8-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
     pickle.dump(history.history, output)
     output.close()
-
-
 
