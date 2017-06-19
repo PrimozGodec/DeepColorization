32edd5cb85b0d7df4a4159df72094d4ac6758b61
primozgodec <p.godec9@gmail.com>
2017-06-19 11:53:13 +0200

    Improved implementation9-bn

--- implementations/implementation9-bn.py
+++ implementations/implementation9-bn.py
@@ -162,13 +162,13 @@ for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch)
     print("START", i * save_every_n_epoch, "/", n_epochs)
     history = model.fit_generator(g, steps_per_epoch=60000/b_size, epochs=save_every_n_epoch,
                                   validation_data=gval, validation_steps=(1024//b_size))
-    model.save_weights("../weights/implementation9-relu-" + str(i * save_every_n_epoch) + ".h5")
+    model.save_weights("../weights/implementation9-bn-" + str(i * save_every_n_epoch) + ".h5")
 
     # save sample images
-    whole_image_check_overlapping(model, 40, "imp9-relu-" + str(i * save_every_n_epoch) + "-")
+    whole_image_check_overlapping(model, 40, "imp9-bn-" + str(i * save_every_n_epoch) + "-")
 
     # save history
-    output = open('../history/imp9-relu-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
+    output = open('../history/imp9-bn-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
     pickle.dump(history.history, output)
     output.close()
 
