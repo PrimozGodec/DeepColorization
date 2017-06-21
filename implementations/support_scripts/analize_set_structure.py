dcda4e34c77ab6be064664428359282b8f02d9f6
primozgodec <p.godec9@gmail.com>
2017-06-21 09:43:49 +0200

    analize dset structure

---
+++ implementations/support_scripts/analize_set_structure.py
@@ -0,0 +1,25 @@
+import os
+from operator import itemgetter
+
+set_dir = "../../../subset100_000"
+
+# train
+print("train\n-----")
+with open(os.path.join(set_dir, "train.txt")) as f:
+    print("File - num of lines: ", len(f.readlines()))
+print("Dir - num of files: ", len(os.listdir(os.path.join(set_dir, "train"))))
+
+# find duplicates
+f = os.listdir(os.path.join(set_dir, "train"))
+from collections import Counter
+co = dict(Counter(f))
+print("Duplicates: ", [(f, c) for f, c in co.items() if c > 1])
+print("Num unique: ", len(co.keys()))
+
+# list groups
+f = [x[:9] for x in os.listdir(os.path.join(set_dir, "train"))]
+
+co = dict(Counter(f))
+print("Groups: ", sorted(dict.items(), key=itemgetter(1))[:15])
+print("Num unique: ", len(co.keys()))
+
