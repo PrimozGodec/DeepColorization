484ffceacd075bd3fe1dc1822d4af025359466f9
primozgodec <p.godec9@gmail.com>
2017-06-21 09:53:49 +0200

    File structure writer

--- implementations/support_scripts/write_names_file.py
+++ implementations/support_scripts/write_names_file.py
@@ -6,4 +6,4 @@ for set in ["train", "validation"]:
     with open(os.path.join(dir_to, set + ".txt"), "w") as handle:
         files = os.listdir(os.path.join(dir_to, set))
         for it in files:
-            print(it, f=handle)
+            print(it, file=handle)
