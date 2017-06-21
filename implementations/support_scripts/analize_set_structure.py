f674fe191286bc19976480ca7ca9947d55064049
primozgodec <p.godec9@gmail.com>
2017-06-21 09:47:16 +0200

    analize dset structure

--- implementations/support_scripts/analize_set_structure.py
+++ implementations/support_scripts/analize_set_structure.py
@@ -23,3 +23,23 @@ co = dict(Counter(f))
 print("Groups: ", sorted(co.items(), key=itemgetter(1))[-15:])
 print("Num unique: ", len(co.keys()))
 
+# train
+print("validation\n-----")
+with open(os.path.join(set_dir, "validation.txt")) as f:
+    print("File - num of lines: ", len(f.readlines()))
+print("Dir - num of files: ", len(os.listdir(os.path.join(set_dir, "validation"))))
+
+# find duplicates
+f = os.listdir(os.path.join(set_dir, "validation"))
+
+co = dict(Counter(f))
+print("Duplicates: ", [(f, c) for f, c in co.items() if c > 1])
+print("Num unique: ", len(co.keys()))
+
+# list groups
+f = [x[:9] for x in os.listdir(os.path.join(set_dir, "validation"))]
+
+co = dict(Counter(f))
+print("Groups: ", sorted(co.items(), key=itemgetter(1))[-15:])
+print("Num unique: ", len(co.keys()))
+
