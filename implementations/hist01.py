1dd5e9a6333feeff8c9a490cbdc2e870da490a3f
primozgodec <p.godec9@gmail.com>
2017-06-24 17:15:25 +0200

    Hist1 ready to test

--- implementations/hist01.py
+++ implementations/hist01.py
@@ -113,12 +113,12 @@ model.compile(optimizer=opt, loss=custom_kullback_leibler_divergence)
 
 model.summary()
 
-start_from = 5
+start_from = 30
 save_every_n_epoch = 5
 n_epochs = 10000
 
 print("weights loaded")
-model.load_weights("../weights/hist01-0.h5")
+model.load_weights("../weights/hist01-25.h5")
 
 # start image downloader
 # ip = ImagePacker("../small_dataset", "../h5_data",  "imp7d-", num_images=1024, num_files=None)
@@ -130,7 +130,7 @@ gval = h5_small_vgg_generator_onehot(b_size, "../h5_validate", None)
 
 
 for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
-
+    whole_image_check_hist(model, 20, "hist01-" + str(i * save_every_n_epoch) + "-")
     print("START", i * save_every_n_epoch, "/", n_epochs)
     history = model.fit_generator(g, steps_per_epoch=60000/b_size, epochs=save_every_n_epoch,
                                   validation_data=gval, validation_steps=(1024//b_size))
