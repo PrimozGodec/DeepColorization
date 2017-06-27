1fda1f40f1cbc2999dde5c5d4f19e7585e33f35f
primozgodec <p.godec9@gmail.com>
2017-06-27 10:01:45 +0200

    Vgg ready to tes

--- implementations/vgg01.py
+++ implementations/vgg01.py
@@ -2,6 +2,8 @@ import pickle
 import sys
 import os
 
+from implementations.support_scripts.image_tester import image_error_vgg
+
 sys.path.append(os.getcwd()[:os.getcwd().index('implementations')])
 
 from implementations.support_scripts.metrics import root_mean_squared_error, mean_squared_error
@@ -68,26 +70,27 @@ model.summary()
 start_from = 0
 save_every_n_epoch = 1
 n_epochs = 10000
-# model.load_weights("../weights/vgg1-0.h5")
+model.load_weights("../weights/vgg1-5.h5")
 
 # start image downloader
 ip = None
 
-g = h5_vgg_generator(b_size, "../data/h5_224_train", ip)
-gval = h5_vgg_generator(b_size, "../data/h5_224_validation", None)
+# g = h5_vgg_generator(b_size, "../data/h5_224_train", ip)
+# gval = h5_vgg_generator(b_size, "../data/h5_224_validation", None)
+#
+#
+# for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
+#     print("START", i * save_every_n_epoch, "/", n_epochs)
+#     history = model.fit_generator(g, steps_per_epoch=100000//b_size, epochs=save_every_n_epoch,
+#                                   validation_data=gval, validation_steps=(10000//b_size))
+#     model.save_weights("../weights/vgg1-" + str(i * save_every_n_epoch) + ".h5")
+#
+#     # save sample images
+#     image_check(model, 80, "vgg1-" + str(i * save_every_n_epoch) + "-")
+#
+#     # save history
+#     output = open('../history/vgg1-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
+#     pickle.dump(history.history, output)
+#     output.close()
 
-
-for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
-    print("START", i * save_every_n_epoch, "/", n_epochs)
-    history = model.fit_generator(g, steps_per_epoch=100000//b_size, epochs=save_every_n_epoch,
-                                  validation_data=gval, validation_steps=(10000//b_size))
-    model.save_weights("../weights/vgg1-" + str(i * save_every_n_epoch) + ".h5")
-
-    # save sample images
-    image_check(model, 80, "vgg1-" + str(i * save_every_n_epoch) + "-")
-
-    # save history
-    output = open('../history/vgg1-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
-    pickle.dump(history.history, output)
-    output.close()
-
+image_error_vgg(model, "vgg-test-100", b_size=b_size)
