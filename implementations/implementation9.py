d308bafdc281b81b84800e50d6909fd824855b32
primozgodec <p.godec9@gmail.com>
2017-06-27 09:52:32 +0200

    imp9 ready to test

--- implementations/implementation9.py
+++ implementations/implementation9.py
@@ -2,6 +2,8 @@ import pickle
 import sys
 import os
 
+from implementations.support_scripts.image_tester import image_error_small_vgg
+
 sys.path.append(os.getcwd()[:os.getcwd().index('implementations')])
 
 from implementations.support_scripts.metrics import root_mean_squared_error, mean_squared_error
@@ -109,26 +111,28 @@ model.summary()
 start_from = 0
 save_every_n_epoch = 1
 n_epochs = 10000
-# model.load_weights("../weights/implementation9-39.h5")
+model.load_weights("../weights/implementation9-12.h5")
 
 # start image downloader
 ip = None
 
-g = h5_small_vgg_generator(b_size, "../data/h5_small_train", ip)
-gval = h5_small_vgg_generator(b_size, "../data/h5_small_validation", None)
+# g = h5_small_vgg_generator(b_size, "../data/h5_small_train", ip)
+# gval = h5_small_vgg_generator(b_size, "../data/h5_small_validation", None)
+#
+#
+# for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
+#     print("START", i * save_every_n_epoch, "/", n_epochs)
+#     history = model.fit_generator(g, steps_per_epoch=100000//b_size, epochs=save_every_n_epoch,
+#                                   validation_data=gval, validation_steps=(10000//b_size))
+#     model.save_weights("../weights/implementation9-" + str(i * save_every_n_epoch) + ".h5")
+#
+#     # save sample images
+#     whole_image_check_overlapping(model, 80, "imp9-" + str(i * save_every_n_epoch) + "-")
+#
+#     # save history
+#     output = open('../history/imp9-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
+#     pickle.dump(history.history, output)
+#     output.close()
 
-
-for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch):
-    print("START", i * save_every_n_epoch, "/", n_epochs)
-    history = model.fit_generator(g, steps_per_epoch=100000//b_size, epochs=save_every_n_epoch,
-                                  validation_data=gval, validation_steps=(10000//b_size))
-    model.save_weights("../weights/implementation9-" + str(i * save_every_n_epoch) + ".h5")
-
-    # save sample images
-    whole_image_check_overlapping(model, 80, "imp9-" + str(i * save_every_n_epoch) + "-")
-
-    # save history
-    output = open('../history/imp9-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
-    pickle.dump(history.history, output)
-    output.close()
+image_error_small_vgg(model, "imp09-test-100")
 
