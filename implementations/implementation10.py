708b9ead522fbe797c7fcc3af261fc3f3ceaddd4
primozgodec <p.godec9@gmail.com>
2017-06-29 16:52:34 +0200

    imp10 gpu 6

--- implementations/implementation10.py
+++ implementations/implementation10.py
@@ -15,7 +15,7 @@ from keras import backend as K, Input
 from keras import optimizers
 from keras.layers import Conv2D, Lambda, Dense, concatenate, regularizers, add, Conv2DTranspose, MaxPooling2D
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 b_size = 32
 
