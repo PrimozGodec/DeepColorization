416d42dd5ba456e0682d7a0d40ac4e804f4f2d9a
primozgodec <p.godec9@gmail.com>
2017-06-29 21:03:59 +0200

    Implemented cur decomposition on results

--- implementations/analysis/cur_analysis.py
+++ implementations/analysis/cur_analysis.py
@@ -1,7 +1,8 @@
 import numpy as np
 import os
 import pickle
-import Orange
+from Orange.projection import CUR
+from Orange.data import Table
 
 """ Construct RMSE matrix M """
 
@@ -23,4 +24,13 @@ for i, rmse_d in enumerate(rmse_data):
     M[i, :] = rmses_per_method
 
 """ Perform CUR """
+data = Table(M)
+print(data)
+cur = CUR(rank=7, max_error=2, compute_U=False)
+cur_model = cur(data)
 
+transformed_data = cur_model(data, axis=1)
+a = np.argmax(transformed_data, axis=1)
+print(transformed_data)
+
+print(a)
