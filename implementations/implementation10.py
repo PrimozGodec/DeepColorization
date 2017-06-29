8e3feda977495c819edadb19607b72238868e49d
primozgodec <p.godec9@gmail.com>
2017-06-29 09:32:20 +0200

    Implementation 10 - some renames

--- implementations/implementation10.py
+++ implementations/implementation10.py
@@ -103,13 +103,13 @@ for i in range(start_from // save_every_n_epoch, n_epochs // save_every_n_epoch)
     print("START", i * save_every_n_epoch, "/", n_epochs)
     history = model.fit_generator(g, steps_per_epoch=100000//b_size, epochs=save_every_n_epoch,
                                   validation_data=gval, validation_steps=(10000//b_size))
-    model.save_weights("../weights/implementation9-" + str(i * save_every_n_epoch) + ".h5")
+    model.save_weights("../weights/implementation10-" + str(i * save_every_n_epoch) + ".h5")
 
     # save sample images
-    whole_image_check_overlapping(model, 80, "imp9-" + str(i * save_every_n_epoch) + "-")
+    whole_image_check_overlapping(model, 80, "imp10-" + str(i * save_every_n_epoch) + "-")
 
     # save history
-    output = open('../history/imp9-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
+    output = open('../history/imp10-{:0=4d}.pkl'.format(i * save_every_n_epoch), 'wb')
     pickle.dump(history.history, output)
     output.close()
 
