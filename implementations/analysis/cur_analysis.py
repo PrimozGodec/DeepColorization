8a2b162766b83b7f5ec32a79540e3e561a324542
primozgodec <p.godec9@gmail.com>
2017-06-30 09:38:40 +0200

    Data saved to use in orange

--- implementations/analysis/cur_analysis.py
+++ implementations/analysis/cur_analysis.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import pickle
 from Orange.projection import CUR, PCA
-from Orange.data import Table
+from Orange.data import Table, Domain, ContinuousVariable
 from scipy import linalg
 
 """ Construct RMSE matrix M """
@@ -52,3 +52,15 @@ A = np.zeros((num_files, len(selected_images_ids)))
 for i, rmse_d in enumerate(rmse_data):
     rmses_per_method = [rmse_d["rmses"][list(images)[x]] for x in selected_images_ids]
     A[i, :] = rmses_per_method
+
+A = A.T  # to get methods as a feature
+
+print(A.shape)
+
+""" Pack as a orange table """
+domain = Domain([ContinuousVariable(alg) for alg in rmse_files])
+table = Table(domain, A)
+# print(table.domain)
+
+""" Save data """
+table.save("../../processed_data/top99.tab")
