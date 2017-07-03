31b90584d1e7bf58192ee6741109ad8cb13054d1
primozgodec <p.godec9@gmail.com>
2017-07-03 15:02:45 +0200

    Implemented comparison of the methods

--- implementations/analysis/cur_analysis.py
+++ implementations/analysis/cur_analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 import pickle
+
+import scipy.stats
 from Orange.projection import CUR, PCA
 from Orange.data import Table, Domain, ContinuousVariable, DiscreteVariable, StringVariable
 from scipy import linalg
@@ -24,49 +26,73 @@ for i, rmse_d in enumerate(rmse_data):
     rmses_per_method = [rmse_d["rmses"][x] for x in images]
     M[i, :] = rmses_per_method
 
+# rank values in M
+array = np.array(M)
+order = array.argsort(axis=1)
+ranks = order.argsort(axis=1)
+
+# print(ranks[:, :10])
+
+rho, p = scipy.stats.spearmanr(M, axis=1)
+
+# print(rho)
+print(" ".join(["".ljust(10)] + ["%s" % v[:10] for v in rmse_files]))
+for i in range(0, len(rho)):
+    print(" ".join([rmse_files[i][:10]] + [("%.4f" % v).ljust(10) for v in rho[i, :]]))
+
+""" pack rho values to orange data table to perform MDS """
+domain = Domain([ContinuousVariable(alg) for alg in rmse_files], metas=[StringVariable("Methods")])
+table = Table(domain, rho, metas=[[r] for r in rmse_files])
+table.save("../../processed_data/spearman_rhos.tab")
+
+""" pack matrix m to table """
+domain = Domain([ContinuousVariable(alg) for alg in rmse_files], metas=[StringVariable("Images")])
+table = Table(domain, M.T, metas=[[r] for r in images])
+table.save("../../processed_data/m_10000_im.tab")
+
 """ Perform CUR """
-data = Table(M)
-# print(data)
-cur = CUR(rank=7, max_error=1, compute_U=False)
-cur_model = cur(data)
-
-transformed_data = cur_model(data, axis=1)
-a = np.argmax(transformed_data, axis=1)
-# print(transformed_data)
-
-# print(a)
-
-""" SVD """
-U, s, Vh = linalg.svd(M, full_matrices=False)
-print(U.shape, s.shape, Vh.shape)
-maxs = np.argmax(Vh, axis=1)
-
-top = Vh.argsort(axis=1)
-# print(top[:, -5:])
-
-selected_images_ids = top[:, -9:].flatten()
-print(len(selected_images_ids))
-
-""" make error matrix for selected """
-A = np.zeros((num_files, len(selected_images_ids)))
-for i, rmse_d in enumerate(rmse_data):
-    rmses_per_method = [rmse_d["rmses"][list(images)[x]] for x in selected_images_ids]
-    A[i, :] = rmses_per_method
-
-A = A.T  # to get methods as a feature
-selected_names = [list(images)[x] for x in selected_images_ids]
-print(len(set(selected_names)))  # check unique
-
-print(A.shape)
-
-""" Pack as a orange table """
-domain = Domain([ContinuousVariable(alg) for alg in rmse_files], metas=[StringVariable("Image")])
-table = Table(domain, A, metas=np.array(selected_names)[:, np.newaxis])
-
-domain_methods = Domain([ContinuousVariable(im) for im in selected_names], metas=[StringVariable("Method")])
-table_methods = Table(domain_methods, A.T, metas=np.array(rmse_files)[:, np.newaxis])
-# print(table.domain)
-
-""" Save data """
-table.save("../../processed_data/top99-images.tab")
-table_methods.save("../../processed_data/top99-methods.tab")
+# data = Table(M)
+# # print(data)
+# cur = CUR(rank=7, max_error=1, compute_U=False)
+# cur_model = cur(data)
+#
+# transformed_data = cur_model(data, axis=1)
+# a = np.argmax(transformed_data, axis=1)
+# # print(transformed_data)
+#
+# # print(a)
+#
+# """ SVD """
+# U, s, Vh = linalg.svd(M, full_matrices=False)
+# print(U.shape, s.shape, Vh.shape)
+# maxs = np.argmax(Vh, axis=1)
+#
+# top = Vh.argsort(axis=1)
+# # print(top[:, -5:])
+#
+# selected_images_ids = top[:, -9:].flatten()
+# print(len(selected_images_ids))
+#
+# """ make error matrix for selected """
+# A = np.zeros((num_files, len(selected_images_ids)))
+# for i, rmse_d in enumerate(rmse_data):
+#     rmses_per_method = [rmse_d["rmses"][list(images)[x]] for x in selected_images_ids]
+#     A[i, :] = rmses_per_method
+#
+# A = A.T  # to get methods as a feature
+# selected_names = [list(images)[x] for x in selected_images_ids]
+# print(len(set(selected_names)))  # check unique
+#
+# print(A.shape)
+#
+# """ Pack as a orange table """
+# domain = Domain([ContinuousVariable(alg) for alg in rmse_files], metas=[StringVariable("Image")])
+# table = Table(domain, A, metas=np.array(selected_names)[:, np.newaxis])
+#
+# domain_methods = Domain([ContinuousVariable(im) for im in selected_names], metas=[StringVariable("Method")])
+# table_methods = Table(domain_methods, A.T, metas=np.array(rmse_files)[:, np.newaxis])
+# # print(table.domain)
+#
+# """ Save data """
+# table.save("../../processed_data/top99-images.tab")
+# table_methods.save("../../processed_data/top99-methods.tab")
--- implementations/support_scripts/video_packer.py
+++ implementations/support_scripts/video_packer.py
@@ -9,14 +9,11 @@ from random import randint
 
 import h5py
 import numpy as np
-import skvideo.io
-from skimage import color
+
 import time
-import pickle
 
 
 sys.path.append(os.getcwd()[:os.getcwd().index('implementations')])
-from implementations.support_scripts.image_processing import resize_rgb
 
 num_neighbours = 4
 
@@ -32,55 +29,32 @@ def video2h5(from_dir, to_dir, images_per_file, num_files, name):
     # list files to pack
     video_files = os.listdir(from_dir)
 
-    # make file with video lengths
-    file_save_lengths = "../../processed_data/video_frame_num_%s.pkl" % name
-    if not os.path.isfile(file_save_lengths):
-        # obtain num frames for each video
-        num_fr = dict()
-        for v in video_files:
-            r = skvideo.io.FFmpegReader(os.path.join(from_dir, v))
-            num_fr[v] = r.getShape()[0]
-            print(v)
-        with open(file_save_lengths, "wb") as f:
-            pickle.dump(num_fr, f)
-
-    with open(file_save_lengths, "rb") as f:
-        num_fr = pickle.load(f)
-
-    # create empty list for frames
-
-    n = 0
-
     # for each h5 file
     for file_n in range(num_files):
+        t = time.time()
         frames_l = np.zeros((images_per_file, 224, 224, 9))
         frames_ab = np.zeros((images_per_file, 224, 224, 2))
 
         # make random choice of video files to be packed
         selected_videos = np.random.choice(video_files, 1024)
-        for i, video in enumerate(selected_videos.tolist()):
-            t = time.time()
-            file_path = os.path.join(from_dir, video)
+        for i, file_from in enumerate(selected_videos.tolist()):
+            file_path = os.path.join(from_dir, file_from)
+            f = h5py.File(file_path, 'r')
+            images = f["im"]
+            T = images.shape[0]
 
-            T = num_fr[video]
-            r_frame = randint(0, T-1-(num_neighbours * 2 - 1))
-            vid = skvideo.io.vread(file_path, outputdict={"-vf": "select=gte(n\,%d)" % r_frame}, num_frames=num_neighbours*2+1)
-            print(vid.shape)
-            print("a", time.time() - t)
-            # resize images
-            lab_vid = np.zeros((vid.shape[0], 224, 224, 3))
-            for frame_num in range(vid.shape[0]):
-                rgb = resize_rgb(vid[frame_num], size=(224, 224))
-                lab_vid[frame_num, :, :, :] = color.rgb2lab(rgb)
-            print("b", time.time() - t)
+            r_frame = randint(0, T-1-(num_neighbours * 2 + 1))
+            lab_vid = images[r_frame:r_frame+(num_neighbours*2 + 1)]
+            print(lab_vid.shape)
             # save middle image as ab
             frames_ab[i, :, :, :] = lab_vid[num_neighbours, :, :, 1:]
             stacked_l = np.stack(np.split(lab_vid[:, :, :, 0], 1 + 2 * num_neighbours, axis=0), axis=3)
             frames_l[i, :, :, :] = stacked_l
-            print("c", time.time() - t)
+
 
         save_h5((frames_l, frames_ab), to_dir, file_n)
+        print(time.time() - t)
 
 
 if __name__ == "__main__":
-    video2h5("../../../videos_dataset/training", "../../data/video/training", 1024, 100, "training")
+    video2h5("../../../videos_dataset/training_h5", "../../data/video/training", 1024, 100, "training")
