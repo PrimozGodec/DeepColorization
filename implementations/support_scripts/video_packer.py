ea815632ea953ca129a8ff09da6f5276168c42af
primozgodec <p.godec9@gmail.com>
2017-07-03 21:04:37 +0200

    Video packer file close and removed print

--- implementations/support_scripts/video_packer.py
+++ implementations/support_scripts/video_packer.py
@@ -22,6 +22,7 @@ def save_h5(data, to_dir, im_num):
     f = h5py.File(os.path.join(to_dir, str(im_num) + ".h5"), 'w')
     f.create_dataset('l', l.shape, dtype='float', data=l)
     f.create_dataset('ab', ab.shape, dtype='float', data=ab)
+    f.close()
 
 
 def video2h5(from_dir, to_dir, images_per_file, num_files):
@@ -45,7 +46,7 @@ def video2h5(from_dir, to_dir, images_per_file, num_files):
 
             r_frame = randint(0, T-1-(num_neighbours * 2 + 1))
             lab_vid = images[r_frame:r_frame+(num_neighbours*2 + 1)]
-            print(lab_vid.shape)
+            # print(lab_vid.shape)
             # save middle image as ab
             frames_ab[i, :, :, :] = lab_vid[num_neighbours, :, :, 1:]
             stacked_l = np.stack(np.split(lab_vid[:, :, :, 0], 1 + 2 * num_neighbours, axis=0), axis=3)
