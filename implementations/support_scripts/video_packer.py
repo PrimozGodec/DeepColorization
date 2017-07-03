f11a4aa42f9baa59fd263349e4b94d51e3e215da
primozgodec <p.godec9@gmail.com>
2017-07-03 11:40:52 +0200

    Video packer made to pack full batches

--- implementations/support_scripts/video_packer.py
+++ implementations/support_scripts/video_packer.py
@@ -5,52 +5,82 @@ Write them as h5 file
 """
 import os
 import sys
+from random import randint
 
 import h5py
 import numpy as np
 import skvideo.io
 from skimage import color
+import time
+import pickle
+
 
 sys.path.append(os.getcwd()[:os.getcwd().index('implementations')])
 from implementations.support_scripts.image_processing import resize_rgb
 
+num_neighbours = 4
 
 def save_h5(data, to_dir, im_num):
+    l, ab = data
     f = h5py.File(os.path.join(to_dir, str(im_num) + ".h5"), 'w')
-    f.create_dataset('im', data.shape, dtype='float', data=data)
+    f.create_dataset('l', l.shape, dtype='float', data=l)
+    f.create_dataset('ab', ab.shape, dtype='float', data=ab)
 
 
-def video2h5(from_dir, to_dir, images_per_file):
+def video2h5(from_dir, to_dir, images_per_file, num_files, name):
 
     # list files to pack
     video_files = os.listdir(from_dir)
 
-    # create empty list for rames
-    frames_lab = np.zeros((images_per_file, 224, 224, 3))
-    file_n = 0
+    # make file with video lengths
+    file_save_lengths = "../../processed_data/video_frame_num_%s.pkl" % name
+    if not os.path.isfile(file_save_lengths):
+        # obtain num frames for each video
+        num_fr = dict()
+        for v in video_files:
+            r = skvideo.io.FFmpegReader(os.path.join(from_dir, v))
+            num_fr[v] = r.getShape()[0]
+            print(v)
+        with open(file_save_lengths, "wb") as f:
+            pickle.dump(num_fr, f)
+
+    with open(file_save_lengths, "rb") as f:
+        num_fr = pickle.load(f)
+
+    # create empty list for frames
+
     n = 0
 
-    for i, video_file in enumerate(video_files):
-        vid = skvideo.io.vreader(os.path.join(from_dir, video_file))
-        for frame in vid:
-            rgb = resize_rgb(frame, size=(224, 224))
-            lab = color.rgb2lab(rgb)
+    # for each h5 file
+    for file_n in range(num_files):
+        frames_l = np.zeros((images_per_file, 224, 224, 9))
+        frames_ab = np.zeros((images_per_file, 224, 224, 2))
 
-            frames_lab[n, :, :, :] = lab
-            n += 1
+        # make random choice of video files to be packed
+        selected_videos = np.random.choice(video_files, 1024)
+        for i, video in enumerate(selected_videos.tolist()):
+            t = time.time()
+            file_path = os.path.join(from_dir, video)
 
-            if n >= images_per_file:
-                save_h5(frames_lab, to_dir, file_n)
-                file_n += 1
-                n = 0
+            T = num_fr[video]
+            r_frame = randint(0, T-1-(num_neighbours * 2 - 1))
+            vid = skvideo.io.vread(file_path, outputdict={"-vf": "select=gte(n\,%d)" % r_frame}, num_frames=num_neighbours*2+1)
+            print(vid.shape)
+            print("a", time.time() - t)
+            # resize images
+            lab_vid = np.zeros((vid.shape[0], 224, 224, 3))
+            for frame_num in range(vid.shape[0]):
+                rgb = resize_rgb(vid[frame_num], size=(224, 224))
+                lab_vid[frame_num, :, :, :] = color.rgb2lab(rgb)
+            print("b", time.time() - t)
+            # save middle image as ab
+            frames_ab[i, :, :, :] = lab_vid[num_neighbours, :, :, 1:]
+            stacked_l = np.stack(np.split(lab_vid[:, :, :, 0], 1 + 2 * num_neighbours, axis=0), axis=3)
+            frames_l[i, :, :, :] = stacked_l
+            print("c", time.time() - t)
 
-        vid.close()
-        print(i)
-
-    # save last data if exist
-    if n >= 0:
-        save_h5(frames_lab[:n, :, :, :], to_dir, file_n)
+        save_h5((frames_l, frames_ab), to_dir, file_n)
 
 
 if __name__ == "__main__":
-    video2h5("../../../videos_dataset/validation", "../../data/video/validation", 1024)
+    video2h5("../../../videos_dataset/training", "../../data/video/training", 1024, 100, "training")
