1bcb0e2e462dd04473b45faa8a7dd3bea51fd206
primozgodec <p.godec9@gmail.com>
2017-07-25 09:38:15 +0200

    Survey script updated

--- implementations/analysis/select_images_survey.py
+++ implementations/analysis/select_images_survey.py
@@ -49,7 +49,27 @@ dir_to = "../../../100selection_survey"
 
 import matplotlib.pyplot as plt
 
-for file_ in os.listdir("../../../EvalueationApp/static/images/imp09-test-full-"):
-    image1 = load_images("../../../EvalueationApp/static/images/imp09-test-full-", file_, size=(224, 224))
+for file_ in os.listdir(os.path.join(dir_to, "hist05-test-full-")):
+    image1 = load_images(os.path.join(dir_to, "hist05-test-full-"), file_, size=(224, 224))
     scipy.misc.toimage(color.lab2rgb(image1), cmin=0.0, cmax=1.0)\
-        .save(os.path.join("../../../EvalueationApp/static/images/imp09-test-full-", file_))
+        .save(os.path.join(dir_to, "hist05-test-full-", file_))
+
+# add other methods
+
+# methods_order_v = {
+#     # "hist02-test-full-": 10,
+#     "hist05-test-full-": 12,
+#     "hyper03-test-full-": 3,
+#     # "imp09-test-full-": 4,
+#     # "imp9-full-test-full-": 7,
+#     # "let-there-color-test-full-": 2,
+#     "vgg-test-full-": 9}
+#
+# for key in methods_order_v.keys():
+#     copy_to = os.path.join(dir_to, key)
+#
+#     if not os.path.isdir(copy_to):
+#         os.makedirs(copy_to)
+#
+#     for file in os.listdir(os.path.join(dir_to, "originals")):
+#         copyfile(os.path.join(dir_from, key + file), os.path.join(copy_to, file))
